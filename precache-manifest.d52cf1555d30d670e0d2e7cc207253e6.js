a21e93c1c8acc0e1641bf157038f92710e983137
hailey822@naver.com <hailey822@naver.com>
2019-10-27 01:39:29 -0400

    Updates

---
+++ precache-manifest.d52cf1555d30d670e0d2e7cc207253e6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6817de3a943d8042e272a50235207c6d",
+    "url": "/Seoul-Culture-City/index.html"
+  },
+  {
+    "revision": "8a0c1d7f4d26bf9483e5",
+    "url": "/Seoul-Culture-City/static/css/main.97fe1d30.chunk.css"
+  },
+  {
+    "revision": "e230bb3aef74060b300d",
+    "url": "/Seoul-Culture-City/static/js/2.3c599014.chunk.js"
+  },
+  {
+    "revision": "8a0c1d7f4d26bf9483e5",
+    "url": "/Seoul-Culture-City/static/js/main.bd651629.chunk.js"
+  },
+  {
+    "revision": "ffb152cd829a89b5396b",
+    "url": "/Seoul-Culture-City/static/js/runtime-main.9b05c66c.js"
+  },
+  {
+    "revision": "4423463dd4f25bfee23c14590d55f728",
+    "url": "/Seoul-Culture-City/static/media/Empty.4423463d.svg"
+  },
+  {
+    "revision": "89a2d1bcbca1cbbbe42b9ede33130efa",
+    "url": "/Seoul-Culture-City/static/media/Heart.89a2d1bc.svg"
+  },
+  {
+    "revision": "1f2f05502715e9e50fa10b6391f54a42",
+    "url": "/Seoul-Culture-City/static/media/apma.1f2f0550.jpg"
+  },
+  {
+    "revision": "53281b0e6c66cfde97460c2f33aba0c0",
+    "url": "/Seoul-Culture-City/static/media/ddp.53281b0e.jpg"
+  },
+  {
+    "revision": "204c5c60f93924dadf63b3cdca013795",
+    "url": "/Seoul-Culture-City/static/media/kukje.204c5c60.jpg"
+  },
+  {
+    "revision": "8f06b17c41f9f9f4fae9054b69409f35",
+    "url": "/Seoul-Culture-City/static/media/leeum.8f06b17c.jpg"
+  },
+  {
+    "revision": "51d501e4119698eaa4295cb594a360e5",
+    "url": "/Seoul-Culture-City/static/media/left.51d501e4.svg"
+  },
+  {
+    "revision": "d423a239f1dd825a87ac875e35951fd0",
+    "url": "/Seoul-Culture-City/static/media/lmoa.d423a239.png"
+  },
+  {
+    "revision": "7b498ab36d40d2857165959d2271bc06",
+    "url": "/Seoul-Culture-City/static/media/mmca.7b498ab3.jpg"
+  },
+  {
+    "revision": "2ffe8d4f168d366815e283803200ec18",
+    "url": "/Seoul-Culture-City/static/media/right.2ffe8d4f.svg"
+  },
+  {
+    "revision": "a6df6d83f11e67d3924c25c4937f8d7f",
+    "url": "/Seoul-Culture-City/static/media/sac.a6df6d83.jpg"
+  },
+  {
+    "revision": "6538f11aa1a2a549eb2e5e1f11648784",
+    "url": "/Seoul-Culture-City/static/media/sema.6538f11a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Seoul-Culture-City/precache-manifest.1e5ef88917c6683e0a27823775c1c2e1.js"
+  "/Seoul-Culture-City/precache-manifest.d52cf1555d30d670e0d2e7cc207253e6.js"
 );
 
 self.addEventListener('message', (event) => {
